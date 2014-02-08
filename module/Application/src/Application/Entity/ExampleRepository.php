28e73813f572c89e7a5490e575da19087abe4ece
Mike Zukowsky <michal@durooil.com>
2014-02-08 22:01:26 +0100

    Update ExampleRepository.php

--- module/Application/src/Application/Entity/ExampleRepository.php
+++ module/Application/src/Application/Entity/ExampleRepository.php
@@ -3,6 +3,16 @@ namespace Application\Entity;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * ExampleRepository class
+ * 
+ * To access the repository from the application:
+ * $sm=$this->getServiceLocator();
+ * $em=$sm->get('Doctrine\ORM\EntityManager');
+ * 
+ * $repository=$em->getRepository('Application\Entity\Example');
+ * echo get_class($repository);
+ */
 class ExampleRepository extends EntityRepository
 {
 
