6e5ac63f244c6d86756d811eae5abed619e2cafc
Mike Zukowsky <michal@durooil.com>
2014-02-11 19:21:42 +0100

    Create Example.php

---
+++ module/Application/src/Application/Repository/Example.php
@@ -0,0 +1,19 @@
+<?php
+namespace Application\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+* ExampleRepository class
+*
+* To access the repository from the application:
+* $sm=$this->getServiceLocator();
+* $em=$sm->get('Doctrine\ORM\EntityManager');
+*
+* $repository=$em->getRepository('Application\Entity\Example');
+* echo get_class($repository);
+*/
+class ExampleRepository extends EntityRepository
+{
+    
+}
